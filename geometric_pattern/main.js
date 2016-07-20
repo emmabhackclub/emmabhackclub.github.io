e7d51f76e22d3306b68fb26734519df15ec6289d
Emma <emblenkinsop@gmail.com>
2016-07-20 19:35:57 +0000

    geometric pattern p5.js

---
+++ geometric_pattern/main.js
@@ -0,0 +1,54 @@
+var NUM_CIRCLES = 12;
+var circleDiameter;
+var circleRadius
+var rVal;
+var gVal;
+var bVal;
+
+function setup() {
+    createCanvas(480, 600);
+    circleDiameter = width/NUM_CIRCLES;
+    circleRadius = circleDiameter/2;
+}
+
+function draw() {
+   var rVal = 255;
+   var gVal = 0;
+   var bVal = 0;
+   
+   var isShifted = false;
+   
+   var y = height;
+   while (y >= 0) {
+      
+       var x;
+       
+       if (isShifted) {
+           x = circleRadius;
+       } else {
+           x = 0;
+       }
+       
+       while (x <= width) {
+           fill(color(rVal, gVal, bVal));
+           stroke(color(rVal, gVal, bVal));
+           ellipse(x, y, circleDiameter, circleDiameter);
+           x = x + circleDiameter;
+           
+                 rVal = rVal - 2;
+      gVal = gVal + 7;
+      bVal = bVal + 3;
+       }
+      y = y - circleRadius;
+      isShifted = !isShifted
+      
+//
+   }
+}
+
+function keyPressed() {
+    if (keyCode === 115 || keyCode === 83) {
+        saveCanvas('geometric pattern', 'png')
+    }
+    return false;
+}
