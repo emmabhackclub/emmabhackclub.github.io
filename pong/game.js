80e668eb66c9b897fc0c32a3ee0e2761727bea66
Emma <emblenkinsop@gmail.com>
2016-07-21 23:52:21 +0000

    bouncing

--- pong/game.js
+++ pong/game.js
@@ -1,32 +1,33 @@
 var player;
 var enemy;
 var ball;
-var ballAngle = 20;
+var ballAngle = 5;
 
 function setup() {
     createCanvas(500, 300);
     enemy = createSprite(5, 75, 10, 60);
     player = createSprite((width-5), 30, 10, 60);
     ball = createSprite((width/2), 30, 10, 10);
-    ball.setSpeed(4, ballAngle);
 }
 
 function draw() {
     background(0, 0, 0);
+    ball.setSpeed(4, ballAngle);
     
   //bouncing 
         //sprites
   if (ball.overlap(player)) {
-      ball.position.x = width - 15;
-      ball.setSpeed(4, (180 - ballAngle));
+      ball.position.x = width - 20;
+      ballAngle = (180 - ballAngle)
+      console.log(ballAngle)
   }
   if (ball.overlap(enemy)) {
-      ball.position.x = 15;
-      ball.setSpeed(4, (ballAngle));
+      ball.position.x = 20;
+      ballAngle = (180 - ballAngle)
   }
         //edges
-    if (ball.position.y = (height-5)) {
-        
+    if (ball.position.y >= (height-20)) {
+        ballAngle = (90 + ballAngle)
     }
   
   //player controls
@@ -38,7 +39,7 @@ function draw() {
   }
   
   //enemy response
-  if (ball.position.y > enemy.position.y) {
+  if ((ball.position.y > enemy.position.y) && enemy.position.y <= (height-30)) {
       enemy.position.y = enemy.position.y + 4
   }
   if (ball.position.y < enemy.position.y) {
