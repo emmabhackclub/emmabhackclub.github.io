29a5d75d9394b1edc4a02f9ca7eff4aa86e5a0a3
Emma <emblenkinsop@gmail.com>
2016-07-23 02:40:12 +0000

    text color, smooth path

--- bear/main.js
+++ bear/main.js
@@ -19,9 +19,12 @@ function setup() {
 
 function draw() {
     textAlign(CENTER);
+    textSize(16)
+    textFont("Georgia");
     //opening level
     if (level == 1) {
         background(backgroundImage1);
+        fill(250, 150, 100);
         if (progression == 0) {
             text("A farmer has a black bear, a deer, and a bush.\n He wants to cross a river.\n The bear, if left alone, will eat the deer.", width / 2, (height / 4));
         }
@@ -29,7 +32,7 @@ function draw() {
             text("The farmer takes the deer across the river.", width / 2, height / 4);
         }
         if (progression == 2) {
-            text("The bear, being an omnivore, eats the bush anyway.", width / 2, height / 4);
+            text("The bear, being an omnivore,\n eats the bush anyway.", width / 2, height / 4);
         }
         if (progression == 3) {
             text("Newly invigorated, the bear swims the river.\n The bear eats the deer.\n Then it eats the farmer.", width / 2, height / 4);
@@ -44,6 +47,7 @@ function draw() {
     //second level correct
     if (level == 2) {
         background(backgroundImage2);
+        fill(200, 100, 0);
         if (progression == 0) {
             text("You forage for berries in the woods.", width / 2, height / 4);
         }
@@ -59,7 +63,8 @@ function draw() {
     }
     //third level correct
     if (level == 3) {
-        background(backgroundImage2);
+        background(backgroundImage3);
+        fill(0);
         if (progression == 0) {
             text("It's incredibly cold in the north,\n and food is hard to find.", width / 2, height / 4);
         }
@@ -75,12 +80,14 @@ function draw() {
     }
     //fourth level correct
     if (level == 4) {
+        console.log("change");
         background(backgroundImage3);
+        fill(0);
         if (progression == 0) {
             text("It's the hardest endeavor of your ursid life,\n but you learn to live in the north.", width / 2, height / 4);
         }
         if (progression == 1) {
-            text("You become one with this harsh and beautiful landscape,\n living with it in perfect harmony.", width / 2, height / 4);
+            text("You become one with this\n harsh and beautiful landscape,\n living with it in perfect harmony.", width / 2, height / 4);
         }
         if (progression == 2) {
             text("You grow stronger, fiercer, bolder.\n You are now:\n\n POLAR BEAR", width / 2, height / 4);
@@ -98,6 +105,7 @@ function draw() {
     //fifth level correct
     if (level == 5) {
         background(backgroundImage4);
+        fill(255);
         if (progression == 0) {
             text("You meditate upon the nature of the universe.", width / 2, height / 4);
         }
@@ -128,13 +136,18 @@ function keyTyped() {
     if (key === 'b' && (level==1) && (progression>= 5)) {
             level = 2;
             progression = 0;
-    } else if (key === 'b' && (level==2) && (progression >= 3)) {
+    }
+    if (key === 'b' && (level==2) && (progression >= 3)) {
             level = 3;
             progression = 0;
-    } else if (key === 'b' && (level==3) && (progression >= 3)) {
+            console.log("level 3");
+    }
+    if (key === 'b' && (level==3) && (progression >= 3)) {
             level = 4;
             progression = 0;
-    } else if (key === 'b' && (level=4) && (progression >= 5)) {
+            console.log("level 4");
+    }
+    if (key === 'b' && (level==4) && (progression >= 5)) {
             level = 5;
             progression = 0;
     }
